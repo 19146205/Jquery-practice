ece4f9ea7d1f21d6cded9f06ac71c5352eb1c2f3
jibril yusuf <jibril_2012@live.com>
2022-01-11 19:53:39 +0000

    code working

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -16,3 +16,17 @@
 // TODO: Append the author element to the quote element
 
 // TODO: Append the quote element to the HTML element with an ID of `root` in `index.html`
+
+
+
+const rootElement = $('#root');
+const newParagraphElement = $('<p>')
+newParagraphElement.text('~ Carol Dweck');
+newParagraphElement.addClass('plain')
+
+const headerElement = $('<h1>')
+headerElement.text('Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning.')
+headerElement.addClass('fancy');
+newParagraphElement.append(headerElement)
+
+rootElement.append(newParagraphElement)
